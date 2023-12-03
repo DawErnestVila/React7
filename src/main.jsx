5f3c7bf2ca9ace2b3cb1b4cfb15c0989d8f4ff2c
prosfp <110301233+prosfp@users.noreply.github.com>
2023-12-03 01:24:16 +0100

    main clean

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Layout from './components/Layout';
-import NouClient from './pages/NouClient';
-import Index, { loader as clientsLoader } from './pages/Index';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Index />,
-        loader: clientsLoader,
-      },
-      {
-        path: '/clients/nou',
-        element: <NouClient />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <h1>DAW MP6 - React</h1>
   </React.StrictMode>,
 );
 
